15fba9afe56f20d59843d77a4fecf0a1a647d849
Your Name <you@example.com>
2025-08-15 00:38:07 +0400

    add FibonacciNumber

---
+++ FibonacciNumber.cs
@@ -0,0 +1,34 @@
+﻿namespace FibonacciNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            string? x = Console.ReadLine();
+            int y = int.Parse(x);
+            int first = 0;
+            int second = 1;
+            if (y <= 0)
+            {
+                Console.WriteLine("number must be positive");
+            }
+            else if (y == 1)
+            {
+                Console.WriteLine(first);
+            }
+            else
+            {
+                Console.WriteLine($"{first} \n{second}");
+                for (int i = 2; i < y; i++)
+                {
+                    int nextNum = first + second;
+                    Console.WriteLine(nextNum);
+                    first = second;
+                    second = nextNum;
+                }
+            }
+
+        }
+    }
+}
