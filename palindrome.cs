4547172e3a1b921693da90590477d082d07fa92a
Your Name <you@example.com>
2025-08-14 23:49:22 +0400

    add palindrome.cs

---
+++ palindrome.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Palindrome
+{
+    static void Main()
+    {
+        //11211
+        string? x = Console.ReadLine();
+        char[] y = x.ToCharArray();
+        Array.Reverse(y);
+        string z = new string(y);
+        bool m = (x == z);
+        Console.WriteLine(m);
+
+    }
+}
