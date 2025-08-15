cea9aba4bf3f07f7b8653c4133bbf9ea3c049342
Your Name <you@example.com>
2025-08-15 22:10:21 +0400

    MergeTwoSortList

---
+++ MergedLists.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp13
+{
+    internal class MergedLists
+    {
+        static void Main(string[] args)
+        {
+            List<int> list1 = new List<int>();
+            List<int> list2 = new List<int>();
+            list1.Add(1);
+            list1.Add(2);
+            list1.Add(3);
+            list2.Add(4);
+            list2.Add(3);
+            list2.Add(5);
+            list1.AddRange(list2);
+            list1.Sort();
+            foreach (int i in list1)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
