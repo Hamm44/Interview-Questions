6159de3877bb79ad0a8e41b932aa1b8616e6578b
Your Name <sixarulidzeluka26@gmail.com>
2025-08-24 23:02:46 +0400

    String Reverse

---
+++ StringReverse.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace ConsoleApp21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter word");
+            string x = Console.ReadLine();
+            string y = reverseWord(x);
+            Console.WriteLine(y);
+
+        }
+        public static string reverseWord(string word)
+        {
+            StringBuilder ne = new StringBuilder();
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                ne.Append(word[i]);
+            }
+            return ne.ToString();
+        }
+    }
+}
