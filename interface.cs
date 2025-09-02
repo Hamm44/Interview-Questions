4a78eca77f49906a330a8633a4359ecb0a5c593d
Your Name <sixarulidzeluka26@gmail.com>
2025-09-02 23:43:28 +0400

    Interface

---
+++ interface.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleApp33
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            numbers number = new numbers(12, 13);
+           
+            Console.WriteLine(number.getArea());
+            Console.WriteLine(number.getPerimeter());
+        }
+    }
+
+    interface IShape
+    {
+        public decimal getArea();
+        public decimal getPerimeter();
+
+    }
+
+    class numbers : IShape 
+    {
+        int Width { get; set; }
+        int Lenght { get; set; }
+        public numbers(int width, int lenght)
+        {
+            Width = width;
+            Lenght = lenght;
+        }
+        public decimal getArea() => Width * Lenght;
+        public decimal getPerimeter() => Width + Lenght;
+        
+
+    }
+}
