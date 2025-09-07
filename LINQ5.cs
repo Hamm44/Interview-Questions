8810a4c71115c568cadaeeb2f8c716bc68281ccc
Your Name <sixarulidzeluka26@gmail.com>
2025-09-08 00:36:44 +0400

    LINQ use .Any()

---
+++ LINQ5.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+namespace ConsoleApp45
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int> { 4, 5, 8, 12, 16, 55, 2, 3 };
+            bool listNumb = list.Any(odd => odd % 2 == 1);
+            Console.WriteLine(listNumb);
+            bool numbList = (from n in list
+                             where n % 2 == 1
+                             select n).Any();
+            Console.WriteLine(numbList);
+        }
+    }
+}
