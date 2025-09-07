fbbd36c79c7e8a0f0808a08944f2132ddc0115b8
Your Name <sixarulidzeluka26@gmail.com>
2025-09-08 00:29:04 +0400

    LINQ Acendind and Descending string list

---
+++ LINQ4.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp44
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string> { "Mercedes", "Porsche", "Subaru", "Koenigsegg" };
+            var sort = list.OrderByDescending(name => name);
+            var sort1 = list.OrderBy(name => name);
+            foreach (var item in sort1)
+            {
+                Console.WriteLine(item);
+            }
+            var Sort = from n in list
+                       orderby n
+                       select n;
+            foreach (var item in sort1)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
