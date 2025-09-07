6e9e8fef39245c822684cb00ebce9c38ea44b3a0
Your Name <sixarulidzeluka26@gmail.com>
2025-09-08 00:04:34 +0400

    LINQ String Filter

---
+++ LINQ2.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp42
+{
+    namespace Luka
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                List<string> list = new List<string> { "Anna", "Banna", "Alex", "Andrea", "Mary" };
+                char letter = 'A';
+                var listString = list.Where(n => n.StartsWith(letter.ToString()));
+                var stringList = from word in list
+                                 where word.StartsWith(letter.ToString())
+                                 select word;
+
+                foreach (var console in listString)
+                {
+                    Console.WriteLine(console);
+                }
+                foreach (var line in stringList)
+                {
+                    Console.WriteLine(line);
+                }
+
+            }
+        }
+    }
+
+}
