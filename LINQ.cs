8a6da12f49bf81f04d82607b15976137d12bb389
Your Name <sixarulidzeluka26@gmail.com>
2025-09-07 23:52:02 +0400

    LINQ both Syntax

---
+++ LINQ.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp41
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] z = { 1, 2, 3, 4, 5 };
+            //var m = z.Where(x => x % 2 == 0);
+            //foreach (var x in m)
+            //{
+            //    Console.WriteLine(x);
+            //}
+            var evenNumbers = from num in z
+                              where num % 2 == 0
+                              select num;
+
+            foreach (var numbs in evenNumbers)
+            {
+                Console.WriteLine(numbs);
+            }
+        }
+    }
+}
